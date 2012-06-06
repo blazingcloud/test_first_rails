975960c9125060b06629feb113d0efacc048ee5c
Fito <fito@paragil.com>
2012-06-06 16:46:31 -0700

    images directory ignored

--- join_htmls.rb
+++
@@ -1,8 +0,0 @@
-DIR = File.dirname(__FILE__)
-html_files = Dir[DIR+'/html/*.html']
-whole_file = File.new("Test_First_With_Rails.html", "w+")
-
-html_files.each do |file|
-  whole_file.write(File.read(file))
-end
-whole_file.close
